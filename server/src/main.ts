0cd4981b85763a4beb18fc70ed0462a9842678dd
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-01-14 23:54:14 +0300

    feat(server): add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,18 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  useHelmet(app);
+
   await app.listen(4000);
-}
+};
+
+const useHelmet = (app: INestApplication) => {
+  app.use(helmet());
+};
+
 bootstrap();
