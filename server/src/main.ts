17b1ecfe9724bac529036a0ba5e55a12f72561a4
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-01-15 00:01:51 +0300

    feat(server): add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,22 +2,39 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
-  useGlobalPipes(app);
   useHelmet(app);
+  setupSwagger(app);
+  useGlobalPipes(app);
 
   await app.listen(4000);
+};
+
+const useHelmet = (app: INestApplication) => {
+  app.use(helmet());
 };
 
 const useGlobalPipes = (app: INestApplication) => {
   app.useGlobalPipes(new ValidationPipe());
 };
 
-const useHelmet = (app: INestApplication) => {
-  app.use(helmet());
+const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Codex API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+  });
+
+  SwaggerModule.setup('api', app, document, {
+    explorer: true,
+  });
 };
 
 bootstrap();
