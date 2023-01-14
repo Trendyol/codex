16af8d59595291a716f872c3cee46144e69ff52a
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-01-14 23:55:56 +0300

    feat(server): add validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
@@ -6,9 +6,14 @@ import { AppModule } from './app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
+  useGlobalPipes(app);
   useHelmet(app);
 
   await app.listen(4000);
+};
+
+const useGlobalPipes = (app: INestApplication) => {
+  app.useGlobalPipes(new ValidationPipe());
 };
 
 const useHelmet = (app: INestApplication) => {
