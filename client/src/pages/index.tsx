61da63bf1429c22fbfcc6967885bb49d3e158be5
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-02-01 22:36:32 +0300

    feat(client): add community and suggestion sections

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
-import Home from '@modules/Home';
+import Dashboard from '@modules/Dashboard';
 
-const HomePage = () => {
+const DashboardPage = () => {
   return (
     <>
       <Head>
-        <title>Codex | Home</title>
+        <title>Codex | Dashboard</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Home />
+      <Dashboard />
     </>
   );
 };
 
-export default HomePage;
+export default DashboardPage;
