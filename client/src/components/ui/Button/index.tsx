9185d5ec9fede092e7512f9b4e24672aea14ed98
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-02-01 20:48:38 +0300

    feat(client): add input and button elements

--- client/src/components/ui/Button/index.tsx
+++ client/src/components/ui/Button/index.tsx
@@ -1,5 +1,37 @@
-const Button = () => {
-  return <div>Button</div>;
+import { cva, cx, type VariantProps } from 'class-variance-authority';
+import { FC, ReactNode } from 'react';
+
+type ButtonProps = {
+  children: ReactNode;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  fluid?: boolean;
+} & VariantProps<typeof buttonVariants>;
+
+const buttonVariants = cva('text-center font-medium', {
+  variants: {
+    intent: {
+      primary: ['bg-primary-100', 'hover:bg-primary-200', 'text-white'],
+      secondary: ['bg-white', 'border', 'border-gray-400', 'hover:bg-gray-100'],
+    },
+    size: {
+      medium: ['text-sm', 'py-3', 'px-4', 'rounded-lg'],
+    },
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+  },
+});
+
+const Button: FC<ButtonProps> = ({ intent, size, fluid, className, children, ...props }) => {
+  const buttonClasses = cx(buttonVariants({ intent, size, className }), fluid ? 'w-full' : '');
+
+  return (
+    <button {...props} className={buttonClasses}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#3b82f6',
+          200: '#2563eb',
+        },
+        secondary: {
+          100: '#6b7280',
+          200: '#4b5563',
+        },
+        background: '#F3F7FF',
+        accent: '#1f2937',
+      },
+    },
   },
   plugins: [],
-}
+};
