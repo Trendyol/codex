d74cda9382e0513f97740940c107157ef2b15b88
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-02-01 23:11:29 +0300

    feat(client): add leaderboard section

--- client/src/modules/Dashboard/index.tsx
+++ client/src/modules/Dashboard/index.tsx
@@ -1,16 +1,18 @@
 import Community from './components/Community';
+import Leaderboard from './components/Leaderboard';
 import List from './components/List';
 import Suggestion from './components/Suggestion';
 
-const Home = () => {
+const Dashboard = () => {
   return (
     <div className="px-6">
       <div className="text-2xl text-primary-400 font-semibold mb-8">Dashboard</div>
       <div className="flex gap-6">
-        <div>
+        <div className='flex flex-1'>
           <List />
         </div>
-        <div className='flex flex-col gap-6'>
+        <div className="flex flex-col gap-6 w-[300px]">
+          <Leaderboard />
           <Community />
           <Suggestion />
         </div>
@@ -19,4 +21,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Dashboard;
