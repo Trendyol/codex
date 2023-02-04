4fadd04a5d2c8c86e140a53fb930078a129f526e
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-02-04 18:37:25 +0300

    feat: add challenge status updater (#28)

--- server/src/modules/challenge/challenge.module.ts
+++ server/src/modules/challenge/challenge.module.ts
@@ -1,5 +1,6 @@
 import { DataModule } from '@data/data.module';
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 
 import { LobbyModule } from '../lobby/lobby.module';
 import { TeamModule } from '../team/team.module';
@@ -7,7 +8,7 @@ import { ChallengeController } from './challenge.controller';
 import { ChallengeService } from './challenge.service';
 
 @Module({
-  imports: [DataModule, TeamModule, LobbyModule],
+  imports: [DataModule, TeamModule, LobbyModule, ScheduleModule.forRoot()],
   providers: [ChallengeService],
   controllers: [ChallengeController],
   exports: [ChallengeService],
