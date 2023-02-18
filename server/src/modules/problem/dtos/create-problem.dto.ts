d788c5601ad5a1ef18f2c2b4f2f79e2711dc00d0
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-02-18 19:35:33 +0300

    feat: create peer server

--- server/src/modules/problem/dtos/create-problem.dto.ts
+++ server/src/modules/problem/dtos/create-problem.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 import { Difficulty } from '../models/enums';
 
