643fbee67ad01834951ef5d76aa5218965a5e44b
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-03-01 22:54:17 +0300

    refactor: remove page titles (#83)

--- client/src/modules/Dashboard/index.tsx
+++ client/src/modules/Dashboard/index.tsx
@@ -6,7 +6,6 @@ import List from './components/List';
 const Dashboard = () => {
   return (
     <>
-      <div className="text-2xl text-primary-400 mb-6">Dashboard</div>
       <div className="flex gap-6 flex-1">
         <List />
         <div className="flex flex-col gap-4 flex-shrink-0 w-sidebar xl:w-[270px] md:hidden">
--- client/src/modules/Practice/index.tsx
+++ client/src/modules/Practice/index.tsx
@@ -7,7 +7,6 @@ const Practice = () => {
 
   return (
     <>
-      <div className="text-2xl text-primary-400 mb-6">Practice</div>
       <div className="flex gap-6 flex-1">
         <Problems />
         <div className="flex flex-col gap-4 flex-shrink-0 w-sidebar xl:w-[270px] md:hidden">
