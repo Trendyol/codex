b713dcf3fa7c56983b0be718806701d4d0210784
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-03-15 19:34:51 +0300

    fix: client build issues (#117)

--- client/src/pages/problem/[problem].tsx
+++ client/src/pages/problem/[problem].tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Problem from '@modules/Problem';
-import DefaultLayout from '@components/layout/default';
+import DefaultLayout from '@components/layout/Default';
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 
