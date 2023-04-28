504b97f84a9f7447f95ca39ef6d81eb91f5b9bc8
kayraberk.tuncer <kayraberk.tuncer@trendyol.com>
2023-04-28 14:09:01 +0300

    chore: add default avatar and name, close #153

--- server/src/modules/auth/models/jwt.ts
+++ server/src/modules/auth/models/jwt.ts
@@ -1,3 +1,4 @@
+import config from '@core/config/configuration';
 import { UserEntity } from "@core/data/entities"
 
 export type BasicJwt = {
@@ -14,7 +15,7 @@ export type BasicJwt = {
 export function convertToUser(jwt: BasicJwt): UserEntity {
   return {
     email: jwt.email || jwt.sub,
-    name: jwt.name,
-    avatar: jwt.avatar
+    name: jwt.name || config.defaultUser.name,
+    avatar: jwt.avatar || config.defaultUser.avatar,
   } as UserEntity
 }
