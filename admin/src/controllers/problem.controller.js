736e138c095ff7aec7b1cf95f1a3a12303a25aa0
erenyener <mehmeterenyener2@gmail.com>
2023-05-12 11:17:51 +0300

    empty modals added

--- admin/src/controllers/problem.controller.js
+++ admin/src/controllers/problem.controller.js
@@ -1,6 +1,18 @@
 function ProblemController() { }
 
 
-ProblemController.prototype.renderAddProblemModal = function(personName) {
-    Logger.log("renderAddProblemModal")
+ProblemController.prototype.renderAddProblemModal = function() {
+
+    const addProblemModalHtml = HtmlService
+        .createTemplateFromFile('src/views/add-new-problem')
+        .evaluate()
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        .getContent();
+
+    const template = HtmlService.createTemplate(addProblemModalHtml)
+        .evaluate()
+        .setWidth(600)
+        .setHeight(550);
+
+    SpreadsheetApp.getUi().showModalDialog(template, 'Add New Problem');
 }
--- admin/src/controllers/test.controller.js
+++ admin/src/controllers/test.controller.js
@@ -1,6 +1,18 @@
 function TestController() { }
 
 
-TestController.prototype.renderAddTestsModal = function(personName) {
+TestController.prototype.renderAddTestsModal = function() {
     Logger.log("renderAddTestsModal")
+    const addNewTestsHtml = HtmlService
+        .createTemplateFromFile('src/views/add-new-tests')
+        .evaluate()
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        .getContent();
+
+    const template = HtmlService.createTemplate(addNewTestsHtml)
+        .evaluate()
+        .setWidth(600)
+        .setHeight(550);
+
+    SpreadsheetApp.getUi().showModalDialog(template, 'Add New Tests');
 }
