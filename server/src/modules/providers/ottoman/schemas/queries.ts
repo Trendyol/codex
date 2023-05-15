81f6d16b7c69f37a555ba123217a232b1b22e4ce
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-05-15 12:42:32 +0300

    fix: remove hard-coded bucket names from custom queries (#175)

--- server/src/modules/providers/ottoman/schemas/queries.ts
+++ server/src/modules/providers/ottoman/schemas/queries.ts
@@ -1,3 +1,7 @@
+import config from '@core/config/configuration';
+
+const bucketName = config.couchbase.bucketName;
+
 const findChallenges = `SELECT q1.id,
 q1.activeParticipants,
 q1.date,
@@ -9,16 +13,16 @@ q1.status,
 q1.teamSize,
 userActiveParticipant,
 userParticipant
-FROM default q1
+FROM ${bucketName} q1
 LET userActiveParticipant = (
 SELECT "true"
-FROM default q2
+FROM ${bucketName} q2
 WHERE q2.type = 'challenge'
  AND q2.id == q1.id
  AND $USER_ID IN q2.activeParticipants),
 userParticipant = (
 SELECT "true"
-FROM default q3
+FROM ${bucketName} q3
 WHERE q3.type = 'challenge'
  AND q3.id == q1.id
  AND $USER_ID IN q3.participants)
@@ -51,22 +55,22 @@ SELECT q1.id,
        userActiveParticipant,
        userParticipant,
        problem
-FROM default q1
+FROM ${bucketName} q1
 LET userActiveParticipant = (
     SELECT "true"
-    FROM default q2
+    FROM ${bucketName} q2
     WHERE q2.type = 'challenge'
         AND q2.id == q1.id
         AND $USER_ID IN q2.activeParticipants),
 userParticipant = (
     SELECT "true"
-    FROM default q3
+    FROM ${bucketName} q3
     WHERE q3.type = 'challenge'
         AND q3.id == q1.id
         AND $USER_ID IN q3.participants),
 problem = (
   SELECT *
-  FROM default q4
+  FROM ${bucketName} q4
   WHERE q4.type = 'problem'
       AND q4.id == q1.problemId)
 WHERE q1.type = 'challenge'
@@ -87,18 +91,18 @@ GROUP BY q1.id,
          problem`;
 
 const findWinners = `
-SELECT MAX([runtime, teamId])[0] as runtime, MAX([runtime, teamId])[1] as teamId FROM default
+SELECT MAX([runtime, teamId])[0] as runtime, MAX([runtime, teamId])[1] as teamId FROM ${bucketName}
 where type = 'submission' and challengeId = $CHALLENGE_ID
 group by teamId order by runtime desc limit 3`;
 
 const findChallengeTeamFinishRankings = `
-SELECT MIN([date, teamId])[0] as date, MIN([date, teamId])[1] as teamId FROM default
+SELECT MIN([date, teamId])[0] as date, MIN([date, teamId])[1] as teamId FROM ${bucketName}
 where type = 'submission' and status = 3 and challengeId = $CHALLENGE_ID
 group by teamId order by date asc
 `;
 
 const addPointsToUser = `
-UPDATE default
+UPDATE ${bucketName}
 SET points = points + $POINTS
 WHERE type = 'user' and id = $USER_ID
 `;
@@ -107,15 +111,15 @@ const findChallengePlacements = `
 SELECT MIN([date, teamId])[0] AS date,
        MIN([date, teamId])[1] AS teamId,
        participants
-FROM default q1
+FROM ${bucketName} q1
 LET team = (
     SELECT participants
-    FROM default q2
+    FROM ${bucketName} q2
     WHERE type = 'team'
         AND id = q1.teamId),
 participants = (
     SELECT *
-    FROM default  where type = 'user' and id in team[0].participants)
+    FROM ${bucketName}  where type = 'user' and id in team[0].participants)
 WHERE type = 'submission'
     AND status = 3
     AND challengeId = $CHALLENGE_ID
@@ -125,21 +129,21 @@ ORDER BY date ASC
 `;
 
 const appendChallengeToUser = `
-Update default set challenges = ARRAY_APPEND(challenges, $CHALLENGE)
+Update ${bucketName} set challenges = ARRAY_APPEND(challenges, $CHALLENGE)
 where type = 'user' and id = $USER_ID
 `;
 
 const appendProblemToUser = `
-Update default set problems = ARRAY_APPEND(problems, $PROBLEM)
+Update ${bucketName} set problems = ARRAY_APPEND(problems, $PROBLEM)
 where type = 'user' and id = $USER_ID
 `;
 
 const findProblems = `
 SELECT *, solved
-FROM default q1
+FROM ${bucketName} q1
 LET solved = (
     SELECT TRUE
-    FROM default q2
+    FROM ${bucketName} q2
     WHERE type = 'user'
         AND q1.id in q2.problems)
 WHERE type = 'problem'
@@ -153,47 +157,47 @@ ARRAY_LENGTH(solvedMedium) AS solvedMedium,
 ARRAY_LENGTH(totalMedium) AS totalMedium,
 ARRAY_LENGTH(solvedHard) AS solvedHard,
 ARRAY_LENGTH(totalHard) AS totalHard
-FROM default q1
+FROM ${bucketName} q1
 LET solvedAll = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND id IN q1.problems),
 totalAll = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'),
 solvedEasy = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND difficulty = 1
  AND id IN q1.problems),
 totalEasy = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND difficulty = 1),
 solvedMedium = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND difficulty = 2
  AND id IN q1.problems),
 totalMedium = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND difficulty = 2),
 solvedHard = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND difficulty = 3
  AND id IN q1.problems),
 totalHard = (
 SELECT *
-FROM default q2
+FROM ${bucketName} q2
 WHERE type = 'problem'
  AND difficulty = 3)
 WHERE type = 'user'
