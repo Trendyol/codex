a1c2bc8ce8004266fc16e62a8cb13381582758bb
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-06-19 00:04:24 +0300

    feat: add voice meter

--- client/src/components/shared/Connection/components/ConnectionPopup/index.tsx
+++ client/src/components/shared/Connection/components/ConnectionPopup/index.tsx
@@ -1,8 +1,9 @@
 import Button from '@components/ui/Button';
 import Popup from '@components/ui/Popup';
-import { FC, useEffect, useRef } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import AccessIndicator from '../AccessIndicator';
+import { AiFillSound } from 'react-icons/ai';
 
 type ConnectionPopupProps = {
   show: boolean;
@@ -20,16 +21,37 @@ const ConnectionPopup: FC<ConnectionPopupProps> = ({
   checkMediaAccess,
 }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [volumeLevel, setVolumeLevel] = useState(0);
 
   const showAccessedMedia = async () => {
-    const constraints = {
-      audio: true,
-      video: true,
-    };
     try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      let maxLevelL = 0;
+      let oldLevelL = 0;
+
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      const audioContext = new AudioContext();
+      const microphone = audioContext.createMediaStreamSource(stream);
+      const processorNode = audioContext.createScriptProcessor(1024, 1, 1);
+
+      microphone.connect(processorNode);
+      processorNode.connect(audioContext.destination);
+      processorNode.onaudioprocess = (event) => {
+        const inputL = event.inputBuffer.getChannelData(0);
+        let instantL = 0.0;
+        let sumL = 0.0;
+
+        for (let i = 0; i < inputL.length; ++i) {
+          sumL += inputL[i] * inputL[i];
+        }
+        instantL = Math.sqrt(sumL / inputL.length);
+        maxLevelL = Math.max(maxLevelL, instantL);
+        instantL = Math.max(instantL, oldLevelL - 0.008);
+        oldLevelL = instantL;
+        setVolumeLevel(instantL / maxLevelL);
+      };
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
+        videoRef.current.volume = 0.4;
         videoRef.current.play();
       }
     } catch (err) {
@@ -47,18 +69,28 @@ const ConnectionPopup: FC<ConnectionPopupProps> = ({
   }, [show]);
 
   return (
-    <Popup title="Check your connection" show={show} onHide={onHide}>
+    <Popup title="Check Your Connection" show={show} onHide={onHide}>
       <div className="flex w-full flex-col items-center justify-center">
         <div className="flex items-center gap-2">
           <AccessIndicator name="Audio" access={hasAudioAccess} />
           <AccessIndicator name="Video" access={hasVideoAccess} />
         </div>
-        <div className="mt-6 h-[280px]">
-          <video className="h-[280px] rounded-md" ref={videoRef}></video>
+        <div className="mt-2 h-[300px]">
+          <div className="flex w-full items-center ">
+            <AiFillSound className="mr-1" size={16} />
+            <div className="h-3 w-full overflow-hidden rounded-md">
+              <div
+                style={{ width: `${volumeLevel * 100}%` }}
+                className="h-3 rounded-md bg-success"
+              ></div>
+            </div>
+          </div>
+          <video muted controls className="mt-2 h-[264px] w-[352px] rounded-md" ref={videoRef}></video>
         </div>
-        <div className="mt-4 text-xs text-secondary-100">
-          You should be able to see and hear yourself. If you are experiencing problems you should
-          check your browser permissions. If problem still exists we recommend using Google Chrome
+        <div className="text-xs text-secondary-100">
+          You should be able to see your video and your voice level. If you are experiencing problems you
+          should check your browser permissions and refresh it. If problem still
+          exists we recommend using Google Chrome
         </div>
         <Button className="mt-1" intent={'text'} onClick={checkMediaAccess}>
           Refresh
