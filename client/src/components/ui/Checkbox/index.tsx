128fe40e07adadf625c2897647291e6c1f71a9b8
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-06-26 12:38:17 +0300

    refactor: update checkbox label sizes

--- client/src/components/ui/Checkbox/index.tsx
+++ client/src/components/ui/Checkbox/index.tsx
@@ -1,4 +1,3 @@
-import { cn } from '@utils/common';
 import { FC, useId } from 'react';
 
 type CheckboxProps = {
@@ -10,7 +9,7 @@ type CheckboxProps = {
 const Checkbox: FC<CheckboxProps> = ({ label, checked, onChange }) => {
   const id = useId();
   return (
-    <div className={cn('flex cursor-pointer items-center gap-2')}>
+    <div className="flex cursor-pointer items-center">
       <input
         className="h-5 w-5 cursor-pointer rounded-[4px] text-primary-100 outline-none transition-all hover:text-primary-200 focus:ring-0"
         checked={checked}
@@ -19,7 +18,7 @@ const Checkbox: FC<CheckboxProps> = ({ label, checked, onChange }) => {
         id={id}
       />
       {label && (
-        <label className="hover: cursor-pointer select-none" htmlFor={id}>
+        <label className="hover: cursor-pointer select-none pl-2" htmlFor={id}>
           {label}
         </label>
       )}
